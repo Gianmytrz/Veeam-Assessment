6462539eb00644e35b1abe65c84712305bb6dfe7
Gianmytrz <116712693+Gianmytrz@users.noreply.github.com>
2022-10-26 09:53:41 +0200

    Add files via upload

---
+++ KillProcessApp/KillProcessService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KillProcessApp
+{
+    public class KillProcessService
+    {
+
+
+
+        public string processName = "";
+        public int maxLifetime = 0;
+        public int monitorFrequency = 0;
+        public System.Threading.Thread WorkThread;
+
+
+        public bool GoOn;
+        public DateTime dateStartApp;
+        public KillProcessService(string processName, int maxLifeTime, int monitorFrequency)
+        {
+            this.processName = processName;
+            this.maxLifetime = maxLifeTime;
+            this.monitorFrequency = monitorFrequency;
+            dateStartApp = DateTime.Now;
+            GoOn = true;
+            WorkThread = new System.Threading.Thread(Work);
+            WorkThread.Start();
+
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+                if (keyinfo.Key == ConsoleKey.Q)
+                {
+                    Console.WriteLine("Q was pressed :  utility stopped");
+                    WorkThread.Interrupt();
+                    Environment.Exit(-1);
+                    
+                }
+
+            }
+            while (true);
+
+        }
+
+        public KillProcessService() { }
+
+
+
+
+
+        public void Work()
+        {
+
+            while (GoOn)
+            {
+                KillProcess(processName);
+
+                System.Threading.Thread.Sleep(Convert.ToInt32(TimeSpan.FromMinutes(monitorFrequency).TotalMilliseconds));
+            }
+
+
+
+        }
+
+        public void KillProcess(string processName)
+        {
+            System.Diagnostics.Process[] processByName = System.Diagnostics.Process.GetProcessesByName(processName);
+
+            if (processByName.Length > 0)
+            {
+                foreach (var process in processByName)
+                {
+                    if (DateTime.Now > dateStartApp.AddMinutes(maxLifetime))
+                    {
+                        process.Kill();
+                        Console.WriteLine($"Process with id {process.Id} was killed ");
+                    }
+                    else
+                        Console.WriteLine($"There are process with process name : {process} to killed but the alive time is not passed");
+                }
+
+            }
+            else
+                Console.WriteLine($"The aren't proces with name {processName}");
+
+
+        }
+    }
+}
---
+++ KillProcessApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace KillProcessApp
+{
+    internal class Program
+    {
+        static Timer myTimer;
+
+        static void Main(string[] args)
+        {
+
+            int maxLifeTime = 0;
+            int monitorFrequency = 0;
+           // var killProcess = new KillProcessService("notepad++", 2, 1);
+
+            var nameProcess = args[0];
+            var tryMaxLife = Int32.TryParse(args[1], out maxLifeTime);
+            var tryMonitorFreq = Int32.TryParse(args[2], out monitorFrequency);
+
+            if (tryMaxLife && tryMonitorFreq)
+            {
+                Console.WriteLine(nameProcess + "   " + maxLifeTime + "  " + monitorFrequency);
+                new KillProcessService(nameProcess, maxLifeTime, monitorFrequency);
+            }
+
+
+
+        }
+
+        
+    }
+}
